c9557138d015aafdda16cfb989640236d2487433
BotSmilee <94220501+BotSmilee@users.noreply.github.com>
2022-03-24 18:06:27 +0500

    Update event-edit-item-view.js

--- src/view/event-edit-item-view.js
+++ src/view/event-edit-item-view.js
@@ -1,5 +1,5 @@
 export const createEventEditItemTemplate = () => (
-    `<li class="trip-events__item">
+  `<li class="trip-events__item">
                 <form class="event event--edit" action="#" method="post">
                   <header class="event__header">
                     <div class="event__type-wrapper">
@@ -140,4 +140,4 @@ export const createEventEditItemTemplate = () => (
                   </section>
                 </form>
               </li>`
-  );
+);
